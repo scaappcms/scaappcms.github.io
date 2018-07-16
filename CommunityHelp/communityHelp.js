3a46180680f642aa386aba9be02b56eac9b6e2e4
Ikey Benzaken <ikey.benz@gmail.com>
2018-07-16 14:08:08 -0400

    Initial Commit

---
+++ CommunityHelp/communityHelp.js
@@ -0,0 +1,65 @@
+const config = {
+    apiKey: "AIzaSyAtnW8LCJ_4q_GFIB_m9aOk0zEt_o3dPcM",
+    authDomain: "sca-app-cms.firebaseapp.com",
+    databaseURL: "https://sca-app-cms.firebaseio.com",
+    projectId: "sca-app-cms",
+    storageBucket: "",
+    messagingSenderId: "970834659998"
+  };
+firebase.initializeApp(config);
+const database = firebase.database();
+document.onload = loadCommunityHelpers();
+
+function loadCommunityHelpers() {
+    $('main').html('');
+    database.ref('CommunityHelpers').once('value', snapshot => {
+        let categories = snapshot.val();
+        for (let categoryID of Object.keys(categories)) {
+            let categoryDivTemplate = Handlebars.compile($('#category-template').html())
+            let categoryDivHTML     = categoryDivTemplate({categoryKey: categoryID, Title: categories[categoryID].Title});
+            $('#main-content').append(categoryDivHTML);
+            console.log(Object.keys(categories[categoryID]));
+            for (let helperID of Object.keys(categories[categoryID])) {
+                
+                if (helperID != "Title") {
+                    console.log(helperID);
+                    let helperDivTemplate = Handlebars.compile($('#communityHelper-template').html());
+                    let helperDivHTML     = helperDivTemplate({categoryKey: categoryID, helperID: helperID , helper: categories[categoryID][helperID]});
+                    $(`#${categoryID}-helpers`).append(helperDivHTML);
+                }
+            }
+        }
+        $('.HelperInput').on('change', updateFieldInDB);
+    });
+}
+
+function updateFieldInDB() {
+    let path = 'CommunityHelpers/' + $(this).attr('id');
+    database.ref(path).set($(this).val());
+}
+
+function addCategory() {
+    let categoryName = $('#newCategoryInput').val();
+    if (categoryName != "") {
+        let snapshot = database.ref('CommunityHelperCategories').push(categoryName);
+        addHelper(snapshot.key);
+        database.ref(`CommunityHelpers/${snapshot.key}/Title`).set(categoryName);
+        loadCommunityHelpers();
+    } else {
+        alert('Please enter the category name before continuing.');
+    }
+}
+function addHelper(categoryID) {
+    database.ref('CommunityHelpers/' + categoryID).push({
+        Name: '----',
+        Email: '----',
+        Phone: '----'
+    });
+}
+function removeHelper(path, name) {
+    if (confirm("You are about to remove " + name + " from community helpers. Are you sure you want to continue?")) {
+        console.log(path);
+        database.ref('CommunityHelpers/' + path).remove();
+        loadCommunityHelpers();
+    }
+}
---
+++ app.js
@@ -0,0 +1,72 @@
+const config = {
+    apiKey: "AIzaSyAtnW8LCJ_4q_GFIB_m9aOk0zEt_o3dPcM",
+    authDomain: "sca-app-cms.firebaseapp.com",
+    databaseURL: "https://sca-app-cms.firebaseio.com",
+    projectId: "sca-app-cms",
+    storageBucket: "",
+    messagingSenderId: "970834659998"
+  };
+firebase.initializeApp(config);
+const database = firebase.database();
+
+function finalizeShulAdd() {
+    let shulName = $('#newShulInput').val();
+    if (shulName != "") {
+        let snapshot = database.ref('Shuls').push(shulName);
+        database.ref('Shul-Schedules/' + snapshot.key).set({
+            ShulTitle: shulName,
+            Address: '---',
+            FriNight: '---',
+            SatMorning: '---',
+            SatAfternoon: '---',
+            WeekMornings: '---',
+            WeekAfternoons: '---'
+        });
+        $('#addShulPopup').hide();
+        $('#newShulInput').val('');
+        loadShulData();
+    } else {
+        alert('Please enter the shul name before continuing.');
+    }
+}
+
+document.onload = loadShulData();
+
+async function loadShulData() {
+    $('main').html('');
+    var shulSchedules = await new Promise(function(resolve, reject) {
+        database.ref('Shul-Schedules').once('value', snapshot => {
+            resolve(snapshot.val());
+        });
+    });
+    if (!shulSchedules) return;
+    for (let key of Object.keys(shulSchedules)) {
+        let data = {
+            ShulKey: key,
+            Name: shulSchedules[key].ShulTitle,
+            Address: shulSchedules[key].Address,
+            FriNight: shulSchedules[key].FriNight,
+            SatMorning: shulSchedules[key].SatMorning,
+            SatAfternoon: shulSchedules[key].SatAfternoon,
+            WeekMornings: shulSchedules[key].WeekMornings,
+            WeekAfternoons: shulSchedules[key].WeekAfternoons
+        }
+        let template = Handlebars.compile($('#shulDiv-template').html());
+        let html     = template(data);
+        $('main').prepend(html);
+        $('.shulScheduleInput').change(updateFieldInDB);
+    }
+    
+}
+function deleteShul(key, name) {
+    if (confirm(`You are about to remove ${name} permanently. Are you sure you want to continue?`)) {
+        database.ref('Shul-Schedules/' + key).remove();
+        database.ref('Shuls/' + key).remove();
+        loadShulData();
+    }
+}
+function updateFieldInDB() {
+    const path = 'Shul-Schedules/' + $(this).attr('id');
+    console.log(path);
+    database.ref(path).set($(this).val());
+}
